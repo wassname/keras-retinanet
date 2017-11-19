932a54ee31834683d9b35374fc52ffe37cd6ec5f
Maarten de Vries <maarten@de-vri.es>
2017-11-19 21:48:43 +0100

    Blacklist keras 2.1.0 ans 2.1.1 in utils/keras_version.

--- keras_retinanet/utils/keras_version.py
+++ keras_retinanet/utils/keras_version.py
@@ -4,17 +4,21 @@ import keras
 import sys
 
 minimum_keras_version = 2, 0, 9
+keras_version_blacklist = [(2, 1, 0), (2, 1, 1)]
 
 def keras_version():
 	return tuple(map(int, keras.__version__.split('.')))
 
 def keras_version_ok():
-	return keras_version() >= minimum_keras_version
+	minimum_ok = keras_version() >= minimum_keras_version
+	blacklisted = keras_version() in keras_version_blacklist
+	return minimum_ok and not keras_version_blacklist
 
 def assert_keras_version():
 	detected = keras.__version__
 	required = '.'.join(map(str, minimum_keras_version))
-	assert(keras_version_ok()), 'You are using keras version {}. The minimum required version is {}.'.format(detected, required)
+	assert(keras_version() >= minimum_keras_version), 'You are using keras version {}. The minimum required version is {}.'.format(detected, required)
+	assert(keras_version() not in keras_version_blacklist), 'You are using keras version {}. This version is known to have problems with keras-retinanet.'.format(detected)
 
 def check_keras_version():
 	try:
