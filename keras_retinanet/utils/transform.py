00b983fe3d473e729543758e71990b6d32380ba0
Maarten de Vries <maarten@de-vri.es>
2017-11-22 18:44:03 +0100

    Add initial utils.transform module.

---
+++ keras_retinanet/utils/transform.py
@@ -0,0 +1,100 @@
+from  keras.preprocessing import transform_matrix_offset_center, apply_transform
+import numpy as np
+
+def rotation(angle):
+    """ Construct a homogeneous 2D rotation matrix.
+    # Arguments
+        angle: the angle in radians
+    # Returns
+        the rotation matrix as 3 by 3 numpy array
+    """
+    return np.array([
+        [np.cos(angle), -np.sin(angle), 0],
+        [np.sin(angle),  np.cos(angle), 0],
+        [0, 0, 1]
+    ])
+
+def random_rotation(max):
+    """ Construct a random rotation between -max and max.
+    # Arguments
+        max: the maximum absolute angle in radians
+    # Returns
+        a homogeneous 3 by 3 rotation matrix
+    """
+    return rotation(np.random.uniform(-max, max))
+
+def translation(translation):
+    """ Construct a homogeneous 2D translation matrix.
+    # Arguments
+        translation: the translation as column vector
+    # Returns
+        the translation matrix as 3 by 3 numpy array
+    """
+    return np.array([
+        [1, 0, translation[0]],
+        [0, 1, translation[1]],
+        [0, 0, 1]
+    ])
+
+def random_translation(max):
+    """ Construct a random 2D translation between -max and max.
+    # Arguments
+        max: the maximum absolute translation as column vector
+    # Returns
+        a homogeneous 3 by 3 translation matrix
+    """
+    return translation(np.random.uniform(-1, 1, (2, 1)) * max)
+
+def shear(amount):
+    """ Construct a homogeneous 2D shear matrix.
+    # Arguments
+        amount: the shear amount
+    # Returns
+        the shear matrix as 3 by 3 numpy array
+    """
+    return np.array([
+        [1, -np.sin(amount), 0],
+        [0,  np.cos(amount), 0],
+        [0, 0, 1]
+    ])
+
+def random_shear(max):
+    """ Construct a random 2D shear matrix with shear angle between -max and max.
+    # Arguments
+        amount: the max shear amount
+    # Returns
+        a homogeneous 3 by 3 shear matrix
+    """
+    return shear(np.random.uniform(-max, max))
+
+def scaling(factor):
+    """ Construct a homogeneous 2D scaling matrix.
+    # Arguments
+        factor: a 2D vector for X and Y scaling
+    # Returns
+        the zoom matrix as 3 by 3 numpy array
+    """
+    return np.array([
+        [zoom[0], 0, 0],
+        [0, zoom[1], 0],
+        [0, 0, 1]
+    ])
+
+def random_scaling(min, max):
+    """ Construct a random 2D scale matrix between -max and max.
+    # Arguments
+        factor: a 2D vector for maximum X and Y scaling
+    # Returns
+        a homogeneous 3 by 3 scaling matrix
+    """
+    return scaling(np.random.uniform(-1, 1, (2, 1)) * max)
+
+def transform_around(transform, center):
+    """ Get a transform applying the given transform with a different origin.
+    # Arguments:
+        transform: the transformation matrix
+        center: the origin of the transformation
+    # Return:
+        translate(center) * transform * translate(-center)
+    """
+    return np.dot(np.dot(translation_matrix(center), transform), transform_matrix(-center))
